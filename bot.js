3cd6f56f06d792c999500a3c00f24b80caa689a7
faisalalotibi511251 <44735340+faisalalotibi511251@users.noreply.github.com>
2018-11-15 23:02:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-﻿const Discord = require('discord.js');
+const Discord = require('discord.js');
 const client = new Discord.Client();
-
+var prefix = "1";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
@@ -14,13 +14,21 @@ var channel = "510241151895470096";//ايدي الروم
     },305);
 })
 
-
 client.on('message', message => {
-if(message.content.startsWith('bank1')) {
-if(message.author.id !== "510252385663320065") return;
-var args = message.content.split(' ').slice(1).join(' ');
-message.channel.send();
-}
-});
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
  
 client.login(process.env.BOT_TOKEN);
