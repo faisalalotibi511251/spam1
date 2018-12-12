1e3c2838746f589ff5b27f194021f7bc23e3ea8d
faisalalotibi511251 <44735340+faisalalotibi511251@users.noreply.github.com>
2018-12-12 12:43:12 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,34 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
-client.on('ready', async() => {
-var server = "510241151895470092"; // ايدي السررفر
-var channel = "510241151895470096";//ايدي الروم
-    setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**hello , hello , hello , hello , hello , hello ,hello ,  hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello , hello**')
-    },305);
-})
-
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "say") {
-if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
+client.on("guildMemberAdd", member => {
+        if(member.guild.id === "522068696416387087") {  // ايدي السيرفر
+  const channel = member.guild.channels.find('id', '522068696416387093'); //ايدي الروم
+if (!channel) return;
+channel.send(`**Ꮃelcome Ꭲo Ꮪerver Cálin..**`)
+}});
 
 
 
